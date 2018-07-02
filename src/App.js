547df61cdc91e325c36b1c906b4b4232d5248074
Gvinod1991 <venkat.godti3@gmail.com>
2018-07-03 00:08:23 +0530

    Nav design renovated

--- src/App.js
+++ src/App.js
@@ -2,116 +2,81 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import IconButton from 'material-ui/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Menu, { MenuItem } from 'material-ui/Menu';
-import Icon from 'material-ui/Icon';
-import Grid from 'material-ui/Grid';
-import Paper from 'material-ui/Paper';
-import { Parallax, Background } from 'react-parallax';
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  flex: {
-    flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  pullRight:
-  {
-    float: 'right',
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-     },
-});
+import {Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 class App extends React.Component  {
-    state = {
-      anchorEl: null,
-    };
-    handleMenu = event => {
-      this.setState({ anchorEl: event.currentTarget });
-    };
-  
-    handleClose = () => {
-      this.setState({ anchorEl: null });
-    };
+   
+  constructor(props) {
+    super(props);
 
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
-    const { classes } = this.props;
-    const {anchorEl } = this.state;
-    const open = Boolean(anchorEl);
-    const image2 = "https://www.bookingjini.in/v3/api/public/uploads/banner.jpg";
     return (
-      <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="title" color="inherit" className={classes.flex}>
-            <img src={logo} className="App-logo" alt="logo" />
-            
-          </Typography>
-          <div className={classes.pullRight}>SOFTGEN</div>
-          <div>
-          <IconButton
-                  aria-owns={open ? 'menu-appbar' : null}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                 >
-                  <AccountCircle />
-          </IconButton>
-          
-          <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={open}
-                 onClose={this.handleClose}
-                >
-                  <MenuItem onClick={this.handleClose}><i className="fa fa-home"></i> Home</MenuItem>
-                  <MenuItem onClick={this.handleClose}><i className="fa fa-eye"></i> View Bookings</MenuItem>
-                  <MenuItem onClick={this.handleClose}><i className="fa fa-bed"></i> SOFTGEN (INDIA)</MenuItem>
-          </Menu>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Parallax bgImage={image2} strength={-100}>
-          <div style={{height: 300}}>
-          </div>
-      </Parallax>
-      <Grid container>
-        <Grid item xs={1} sm={1}>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={1} sm={1}>
-        </Grid>
-      </Grid>     
+    <Container fluid>
+      <div className="header">
+      <Navbar color="light" light expand="md">
+          <NavbarBrand href="/"> <img src={logo} className="App-logo" alt="logo" /></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/about/">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/policies">Policies</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/map">Map</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  <i class="fa fa-hotel fa-2x"></i>
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Hotel 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Hotel 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
+    </Container>
     );
   }
+
 }
 App.propTypes = {
   classes: PropTypes.object.isRequired,
 };
-export default withStyles(styles)(App);
+export default App;
