a70a90da80114ec60028c73bfcd57995242f28ed
Gvinod1991 <venkat.godti3@gmail.com>
2018-07-04 00:04:48 +0530

    Banner Design implemented

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ import {Container,
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem } from 'reactstrap';
+  DropdownItem,Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button  } from 'reactstrap';
 class App extends React.Component  {
    
   constructor(props) {
@@ -31,7 +32,7 @@ class App extends React.Component  {
   }
   render() {
     return (
-    <Container fluid>
+    <Container fluid className="">
       <div className="header">
       <Navbar color="light" light expand="md">
           <NavbarBrand href="/"> <img src={logo} className="App-logo" alt="logo" /></NavbarBrand>
@@ -52,7 +53,7 @@ class App extends React.Component  {
               </NavItem>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-                  <i class="fa fa-hotel fa-2x"></i>
+                  <i className="fa fa-hotel fa-2x"></i>
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>
@@ -70,6 +71,79 @@ class App extends React.Component  {
             </Nav>
           </Collapse>
         </Navbar>
+      </div>
+      <div className="row banner">
+        <button className="btn btn-info banner-button">View Photos</button>
+      </div>
+      <div className="row book-room">
+        <div className="col-md-1">
+        </div>
+        <div className="col-md-6">
+        <Card className="room">
+          <CardBody>
+            <CardTitle><div className="row"><p className="col-md-9">Deluxe Room with lake view</p>
+            <button className="col-md-3 btn btn-info">Room Photo</button></div></CardTitle>
+            <div className="row room-properties">
+              <div className="col-md-6 room-capacity">
+              <span className="category fa fa-male fa-2x"></span> 3 
+              <span className="category fa fa-child fa-2x"></span> 2
+              <span className="category fa fa-bed fa-2x"></span> 2
+              </div>
+              <div className="col-md-6 room-price">
+              <span className="fa fa-inr price-tag"> 2500 Per Room Night</span>
+              </div>
+            </div>
+            <div className="clearfix"></div>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <div className="row ">
+              <div className="col-md-4">
+                ACCOMMODATION TYPE
+              </div>
+              <div className="col-md-4 ">
+                Room
+              </div>
+              <div className="col-md-4 ">
+               Amount
+              </div>
+            </div>
+            <div className="row ">
+              <div className="col-md-4">
+               Room Only
+              </div>
+              <div className="col-md-4 ">
+                2
+              </div>
+              <div className="col-md-4 ">
+               2400
+              </div>
+            </div>
+            <div className="row ">
+              <div className="col-md-4">
+               Room with breakfast 
+              </div>
+              <div className="col-md-4 ">
+                2
+              </div>
+              <div className="col-md-4 ">
+               2400
+              </div>
+            </div>
+          </CardBody>
+        </Card>
+        
+        </div>
+        <div class="col-md-4">
+        <Card>
+          <CardBody>
+            <CardTitle>Calendar</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+         </Card>
+        </div>
+        <div className="col-md-1">
+        </div>
       </div>
     </Container>
     );
