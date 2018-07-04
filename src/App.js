8b578140534084692cf403205f68b46347919b88
Gvinod1991 <venkat.godti3@gmail.com>
2018-07-05 00:03:03 +0530

    Ammenities and google map implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 import logo from './logo.svg';
 import './App.css';
 import PropTypes from 'prop-types';
@@ -13,10 +14,14 @@ import {Container,
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem,Card, CardImg, CardText, CardBody,
+  DropdownItem,Card, CardText, CardBody,
   CardTitle, CardSubtitle, Button  } from 'reactstrap';
+  const MapStaticComponent = ({ text }) => <div>{ text }</div>;
 class App extends React.Component  {
-   
+  static defaultProps = {
+    center: { lat: 40.7446790, lng: -73.9485420 },
+    zoom: 11
+  }
   constructor(props) {
     super(props);
 
@@ -144,6 +149,95 @@ class App extends React.Component  {
         </div>
         <div className="col-md-1">
         </div>
+      </div>
+      <div className="row book-room">
+        <div className="col-md-1">
+        </div>
+        <div className="col-md-6">
+      <Card className="room">
+          <CardBody>
+            <CardTitle>Amenities</CardTitle>
+            <div className="row room-properties">
+              <div className="col-md-8 room-capacity">
+                <div className="amenities-block">
+              <span className="category fa fa-cutlery fa-2x"></span> Resturant
+              <span className="category fa fa-bath fa-2x"></span> Bath Room
+              </div>
+              <div className="amenities-block">
+              <span className="category fa fa-wifi fa-2x"></span> Wifi
+              <span className="category fa fa-cutlery fa-2x"></span> Resturant
+              </div>
+              <div className="amenities-block">
+              <i className="category fa fa-bath fa-2x"></i> Bath Room
+              
+              <i className="category fa fa-wifi fa-2x"></i> Wifi
+              </div>
+              <Button className="btn btn-info">More Amenities</Button>
+              </div>
+              <div className="col-md-4 room-price">
+              <span className="fa fa-singin price-tag">Checkin 12 AM</span>
+              <br/>
+              <span className="fa fa-singout price-tag">CheckOut 12 AM</span>
+              </div>
+            </div>
+            <div className="clearfix"></div>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            
+            
+           </CardBody>
+        </Card>
+        <div className="col-md-1"></div>
+      </div>
+      
+      </div>
+      <div className="row book-room">
+        <div className="col-md-1">
+        </div>
+        <div className="col-md-6">
+          <Card className="room">
+              <CardBody>
+                <CardTitle>About Booking Engine</CardTitle>
+                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+              </CardBody>
+            </Card>
+          </div>
+      <div className="col-md-1"></div>
+      </div>
+      <div className="row book-room">
+        <div className="col-md-1">
+        </div>
+        <div className="col-md-6">
+          <Card className="room">
+              <CardBody>
+                <CardTitle>Rules and Policies</CardTitle>
+                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+              </CardBody>
+            </Card>
+          </div>
+      <div className="col-md-1"></div>
+      </div>
+      <div className="row book-room">
+        <div className="col-md-1">
+        </div>
+        <div className="col-md-6">
+          <Card className="room">
+              <CardBody>
+                <CardTitle>Map</CardTitle>
+                <div className='google-map'>
+        <GoogleMapReact
+          defaultCenter={ this.props.center }
+          defaultZoom={ this.props.zoom }>
+          <MapStaticComponent
+            lat={ 40.7473310 }
+            lng={ -73.8517440 }
+            text={ 'maps' }
+          />
+        </GoogleMapReact>
+      </div>
+                </CardBody>
+            </Card>
+          </div>
+      <div className="col-md-1"></div>
       </div>
     </Container>
     );
